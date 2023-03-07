e2140460568de591f7583a92a1692556698fceb5
SchroederSimon <schroeder.ssimon@gmail.com>
2023-03-07 18:16:25 -0300

    finished api req from news, applying general styles

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -21,29 +21,78 @@ function News() {
     }, []);
 
     return (
-        <div className="news-big-container">
-            <h1>EDUCATION AND JOB MARKET</h1>
-            {    
-                articles.slice(0, 5).map((article) => (
-                    <div className="news"key={article.url}>
-                        <div className="news-container" >
-                            <img src={article.urlToImage} alt=""/>
-                            <div className="news-text">
+        <div className="news-blog-container">
+            <div className="news-left-side">
+                <h1>EDUCATION AND JOB MARKET</h1>
+                {
+                    articles.slice(0, 5).map((article) => (
+                        <div className="news-content" key={article.url} >
+                            <h2>{article.title}</h2>
+                            <span>{article.author}</span>
+                            <img src={article.urlToImage} alt={article.title} />
+                            <p>{article.description}<span><a href={article.url} target="_blank"> Read more</a></span></p>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="blog-right-side">
+                <h1>BLOG POSTS</h1>
+                {
+                    articles.slice(0, 5).map((article) => (
+                        <div className="news-content" key={article.url}>
+                            <img src={article.urlToImage} alt={article.title} />
+                            <div className="news-information">
                                 <span>{article.author}</span>
                                 <h2>{article.title}</h2>
                                 <p>{article.description}<span><a href={article.url} target="_blank">Read more</a></span>
                                 </p>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
-            <div className="blog">
-                <h1>BLOG POSTS</h1>
+                    ))
+                }
             </div>
-        </div >
+        </div>
     )
 }
 
 export default News
 
+{/* <div className="parent-container">
+<div className="news-blog-container">
+    <div className="left-side">
+        <h1>EDUCATION AND JOB MARKET</h1>
+        {
+            articles.slice(0, 5).map((article) => (
+                <div className="news-container" key={article.url}>
+                    <div className="news-content" >
+                        <img src={article.urlToImage} alt={article.title} />
+                        <div className="news-information">
+                            <span>{article.author}</span>
+                            <h2>{article.title}</h2>
+                            <p>{article.description}<span><a href={article.url} target="_blank">Read more</a></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            ))
+        }
+    </div>
+    <div className="right-side">
+        <h1>EDUCATION AND JOB MARKET</h1>
+        {
+            articles.slice(0, 5).map((article) => (
+                <div className="news-container" key={article.url}>
+                    <div className="news-content" >
+                        <img src={article.urlToImage} alt={article.title} />
+                        <div className="news-information">
+                            <span>{article.author}</span>
+                            <h2>{article.title}</h2>
+                            <p>{article.description}<span><a href={article.url} target="_blank">Read more</a></span></p>
+                        </div>
+                    </div>
+                </div>
+            ))
+        }
+    </div>
+</div>
+</div> */}
